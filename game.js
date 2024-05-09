001a3e466ba96ae863a9a61aed5bae9b537c322c
Web-knIts <lakshayseth900@gmail.com>
2024-05-09 13:14:21 +0530

    First Commit

---
+++ game.js
@@ -0,0 +1,83 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function(event) {
+  if (!started) {
+    document.getElementById("level-title").textContent = "Level " + level;
+    nextSequence();
+    started = true;
+  }
+});
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    const userChosenColour = this.getAttribute("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+  });
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    document.body.classList.add("game-over");
+    document.getElementById("level-title").textContent = "Game Over, Press Any Key to Restart";
+
+    setTimeout(function () {
+      document.body.classList.remove("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  document.getElementById("level-title").textContent = "Level " + level;
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  const selectedButton = document.getElementById(randomChosenColour);
+  selectedButton.style.display = "none";
+  setTimeout(() => {
+    selectedButton.style.display = "block";
+  }, 100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  const selectedButton = document.getElementById(currentColor);
+  selectedButton.classList.add("pressed");
+  setTimeout(function () {
+    selectedButton.classList.remove("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
